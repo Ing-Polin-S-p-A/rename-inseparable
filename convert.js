aec32d106b65380b61348b67d116fdbb53064232
Gallo Mauro <mauro.gallo@polin.it>
2022-08-01 12:54:26 +0200

    feat: convert to unique children names

---
+++ convert.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = './steps';
+
+fs.readdir(path, function (err, files) {
+
+    files.forEach(element => {
+        console.log(element)
+        let newFileArray = []; 
+
+        const allFileContents = fs.readFileSync(path + '/' + element, 'utf-8');
+        allFileContents.split(/\r?\n/).forEach(line => {
+            
+            let code = element.replace('.stp', '')
+
+            let n = line.search(/'(.*?)PRT'/gm);
+            if (n != -1) {
+                let newLine = line.replace(/'(.*?)PRT'/gm, `'` + code + "." + "$1" + "PRT'");       
+                newFileArray.push(newLine);
+            } else {
+                newFileArray.push(line);
+            }          
+        });
+
+
+        let newFile = fs.createWriteStream(path + '/new-' + element);
+
+        fs.writeFileSync(path + '/new-' + element, newFileArray.join('\n'));
+
+
+    });
+});
